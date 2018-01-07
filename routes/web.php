0dec69e48ea5262d0099a7985180ad3b914081c5
guogb <guogb@qq.com>
2018-01-07 14:11:37 +0000

    Add .edirorconfig

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+<<<<<<< HEAD
 Route::get('/', 'StaticPagesController@home');
 Route::get('/help', 'StaticPagesController@help');
 Route::get('/about', 'StaticPagesController@about');
+=======
+Route::get('/', function () {
+    return view('welcome');
+});
+>>>>>>> e6de5737c6a1b7d4f7ed8990a4164ec6de2d1ff5
